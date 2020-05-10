1aae3606a996aaeb50d147899262a4fab7d9ca54
hbeeen <tnp.kaewveingchai@gmail.com>
2020-05-10 14:12:50 +0700

    Updates

---
+++ precache-manifest.1455206e26dbde46054f37395640f5a8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81473b4a30a0d12f2619b3ede50b47f5",
+    "url": "/react-proj/index.html"
+  },
+  {
+    "revision": "8ced7c6c49d6cb7a14ec",
+    "url": "/react-proj/static/css/main.82f6a4b1.chunk.css"
+  },
+  {
+    "revision": "99200759eb772b5bcdd9",
+    "url": "/react-proj/static/js/2.215c00ef.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-proj/static/js/2.215c00ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ced7c6c49d6cb7a14ec",
+    "url": "/react-proj/static/js/main.9167cca4.chunk.js"
+  },
+  {
+    "revision": "d68f7f0ced1c3321b061",
+    "url": "/react-proj/static/js/runtime-main.8d1855e5.js"
+  },
+  {
+    "revision": "7f1bc1a8f7e78e2b299d21a8cac56a7d",
+    "url": "/react-proj/static/media/Dipsy.7f1bc1a8.jpg"
+  },
+  {
+    "revision": "f98273371b6066721739b665c0e86b1c",
+    "url": "/react-proj/static/media/Lala.f9827337.jpg"
+  },
+  {
+    "revision": "7627f86d51191d0516eb9af82957f6ea",
+    "url": "/react-proj/static/media/cover.7627f86d.jpg"
+  },
+  {
+    "revision": "b0e5cad99a5952366cef63dc9b854408",
+    "url": "/react-proj/static/media/logo_cut.b0e5cad9.jpg"
+  }
+]);
